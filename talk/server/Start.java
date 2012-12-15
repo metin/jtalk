e4347079d085f3ddb4932b7a9b726bf2cb8d8929
Metin Yorulmaz <metin.y@gmail.com>
2012-12-15 18:19:50 -0500

    Server does no accept parameters anymore

--- talk/server/Start.java
+++ talk/server/Start.java
@@ -9,22 +9,22 @@ public class Start  {
   public static void main(String[] args) {
 
     int portNumber = 1500;
-    switch(args.length) {
-      case 1:
-        try {
-          portNumber = Integer.parseInt(args[0]);
-        }
-        catch(Exception e) {
-          System.out.println("Invalid port number.");
-          System.out.println("Usage is: > java Server [portNumber]");
-          return;
-        }
-      case 0:
-        break;
-      default:
-        System.out.println("Usage is: > java Server [portNumber]");
-        return;
-    }
+    // switch(args.length) {
+    //   case 1:
+    //     try {
+    //       portNumber = Integer.parseInt(args[0]);
+    //     }
+    //     catch(Exception e) {
+    //       System.out.println("Invalid port number.");
+    //       System.out.println("Usage is: > java Server [portNumber]");
+    //       return;
+    //     }
+    //   case 0:
+    //     break;
+    //   default:
+    //     System.out.println("Usage is: > java Server [portNumber]");
+    //     return;
+    // }
     Server server = new Server(portNumber);
     server.bootUp();
   }
